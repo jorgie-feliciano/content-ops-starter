5f3573cc027f1eee3d4c2dde4dc9b335cace0801
jorgie-feliciano <jorgie.feliciano@gmail.com>
2025-10-22 08:58:11 -0400

    Remove formRef to fix build error

--- src/components/blocks/FormBlock/index.tsx
+++ src/components/blocks/FormBlock/index.tsx
@@ -83,7 +83,6 @@ export default function FormBlock(props) {
             method="POST"
             netlify-honeypot="bot-field"
             data-netlify="true
-            ref={formRef}
             data-sb-field-path= {fieldPath}
         >
             <input type="hidden" name="form-name" value="contact" />
